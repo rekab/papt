a53ed6eb0e8727c29853e0a195584729c50d194c
james <james@havidol>
2014-10-07 22:14:17 -0700

    remove cruft; rename myApp to papt; narrow the wordpair box

--- static/app.js
+++ static/app.js
@@ -1,14 +1,13 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('papt', [
   'ngRoute',
   'papt.login',
   'papt.instructions',
   'papt.wordpairs',
   'papt.testinstructions',
   'papt.test',
-  'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/login'});
