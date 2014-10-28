b5c3683b364a7bb8d136dad8b8c67b6dead01709
james <james@havidol>
2014-10-27 22:16:42 -0700

    fix wordpairs test now that there's sound

--- static/view/wordpairs/wordpairs_test.js
+++ static/view/wordpairs/wordpairs_test.js
@@ -5,21 +5,27 @@ describe('papt.wordpairs module', function() {
   beforeEach(module('papt.wordpairs'));
 
   describe('wordpairs controller', function() {
-    var mockScope, mockLocation, mockHttp, mockInterval;
+    var mockScope, mockLocation, mockHttp, mockInterval, mockTimeout;
     var wordpairDataHandler;
 
-    beforeEach(inject(function($rootScope, $location, $httpBackend, $interval, $controller) {
+    beforeEach(inject(function($rootScope, $location, $httpBackend, $interval, $timeout, $controller) {
       mockScope = $rootScope.$new();
       mockLocation = $location;
       mockInterval = $interval;
+      mockTimeout = $timeout;
       mockHttp = $httpBackend;
+
+      mockScope.beep = {
+        playPause: function() { this.played = true; }
+      }
 
       var controller = $controller(
         'WordpairsCtrl',
         { 
           $scope: mockScope,
           $location: mockLocation,
-          $interval: mockInterval
+          $interval: mockInterval,
+          $timeout: mockTimeout
         });
       wordpairDataHandler = mockHttp.when('GET', '/data/wordpairs.json')
         .respond([["foo", "bar"], ["qux", "baz"]]);
@@ -50,14 +56,25 @@ describe('papt.wordpairs module', function() {
       mockInterval.flush(10);
       expect(mockScope.numTicksLeft).toBe(299)
 
-      // Skip ahead to the last tick, expect curPair to change.
+      // Skip ahead to the last tick and send time forward.
       mockScope.numTicksLeft = 1
       mockInterval.flush(10);
+      // Expect the beep to have been played.
+      expect(mockScope.beep.played).toBeTruthy();
+      // Skip ahead some more time for the sound to play.
+      mockTimeout.flush(1000);
       expect(mockScope.curPair).toBe(1);
+
+      // Reset the mock sound.
+      mockScope.beep.played = false;
 
       // Skip ahead again, past the last pair, and expect the view to change.
       mockScope.numTicksLeft = 1
       mockInterval.flush(10);
+      // Expect the beep to have been played.
+      expect(mockScope.beep.played).toBeTruthy();
+      // Skip ahead some more time for the sound to play.
+      mockTimeout.flush(1000);
       expect(mockLocation.path()).toBe('/testinstructions');
     }));
   });
